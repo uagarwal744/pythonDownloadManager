54db60563338dc7e124428f216cd36b4b8cb3744
Utsav Agarwal <uagarwal744@gmail.com>
2016-02-09 21:40:20 +0530

    Backend changes

--- Main_Activity.py
+++ Main_Activity.py
@@ -1,5 +1,6 @@
 import urllib2
 import urllib
+import timeit
 from PyQt4 import QtGui,QtCore
 from download_manager import Ui_MainWindow
 
@@ -12,10 +13,11 @@ class download_manager(QtGui.QMainWindow):
 		super(download_manager,self).__init__()
 		self.dialog=Ui_MainWindow()
 		self.dialog.setupUi(self)
-		print self.dialog.pushButton_3
 		self.dialog.pushButton_3.clicked.connect(self.addDownload)
+		self.dialog.pushButton_16.clicked.connect(self.addDownload2)
 
 	def addDownload(self):
+		start=timeit.default_timer()
 		global urlname
 		global urlsize
 		global urlstatus
@@ -23,11 +25,56 @@ class download_manager(QtGui.QMainWindow):
 		global model
 		global idx
 		url=str(self.dialog.lineEdit.text())
-		print url
-		file_name = url.split('/')[-1]
+		urlname = url.split('/')[-1]
 		response=urllib2.urlopen(url)
-		f=open(file_name,'wb')
+		self.meta=response.info()
+		urlsize=int(self.meta.getheaders('Content-Length')[0])
+		if 1000<urlsize<1000000:
+			urlsize=str(urlsize/1000.0) + " KB"
+		elif 1000000<urlsize<10**9:
+			urlsize=str(1.0*urlsize/10**6) + " MB"
+		else:
+			urlsize=str(1.0*urlsize/10**9) + " GB"
+		self.dialog.label_15.setText(urlsize)
+		self.dialog.label_7.setText(urlname)
+		f=open(urlname,'wb')
 		f.write(response.read())
+		stop=timeit.default_timer()
+		speed=float(urlsize.split(' ')[0])/(stop-start)
+		speed=str(speed) + " " + urlsize.split(' ')[-1] + "/s"
+		self.dialog.label_13.setText("100 %")
+		self.dialog.label_8.setText(speed)
+		self.dialog.label_9.setText(str(stop-start) + " s")
+
+	def addDownload2(self):
+		start=timeit.default_timer()
+		global urlname
+		global urlsize
+		global urlstatus
+		global urltype
+		global model
+		global idx
+		url=str(self.dialog.lineEdit_6.text())
+		urlname = url.split('/')[-1]
+		response=urllib2.urlopen(url)
+		self.meta=response.info()
+		urlsize=int(self.meta.getheaders('Content-Length')[0])
+		if 1000<urlsize<1000000:
+			urlsize=str(urlsize/1000.0) + " KB"
+		elif 1000000<urlsize<10**9:
+			urlsize=str(1.0*urlsize/10**6) + " MB"
+		else:
+			urlsize=str(1.0*urlsize/10**9) + " GB"
+		self.dialog.label_17.setText(urlsize)
+		self.dialog.label_68.setText(urlname)
+		f=open(urlname,'wb')
+		f.write(response.read())
+		stop=timeit.default_timer()
+		speed=float(urlsize.split(' ')[0])/(stop-start)
+		speed=str(speed) + " " + urlsize.split(' ')[-1] + "/s"
+		self.dialog.label_67.setText("100 %")
+		self.dialog.label_69.setText(speed)
+		self.dialog.label_70.setText(str(stop-start))
 
 class Main(QtGui.QMainWindow):
 	def __init__(self):
